4e3249422bfb188aa48ee592ba8f4f45dfb1a356
KeriCarpenterProg <56651871+KeriCarpenterProg@users.noreply.github.com>
2023-02-16 20:19:32 -0800

    one nights work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { Routes, Route } from "react-router-dom";
 import React from "react";
 import "./App.css";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 import CampsiteDirectoryPage from "./pages/CampsiteDirectoryPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -8,7 +11,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CampsiteDirectoryPage />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="contact" element={<ContactPage />}></Route>
+        <Route path="directory" element={<CampsiteDirectoryPage />}></Route>
+      </Routes>
       <Footer />
     </div>
   );
